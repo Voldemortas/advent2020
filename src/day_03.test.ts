b124d688ddf8d873e8b5cbb8ee732025b6f17218
Andrius Simanaitis <eragonas5@gmail.com>
2020-12-03 12:40:05 +0200

    added day 3 part 2

--- src/day_03.test.ts
+++ src/day_03.test.ts
@@ -17,5 +17,16 @@ test('part 1', () => {
 })
 
 test('part 2', () => {
-  expect(part2(input)).toBe(0)
+  const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ]
+  const result = [2, 7, 3, 4, 2]
+  slopes.forEach((slope, i) => {
+    expect(part1(input, slope)).toBe(result[i])
+  })
+  expect(part2(input)).toBe(336)
 })
--- src/day_03.ts
+++ src/day_03.ts
@@ -2,23 +2,30 @@ import * as fs from 'fs'
 
 const input = fs.readFileSync(__dirname + '/day_03.txt', 'utf-8')
 
-export function part1(input: string): number {
+export function part1(input: string, slope = [3, 1]): number {
   const data = input.split('\n').map((e) => e.split(''))
   const height = data.length
   const width = data[0].length
   let current = [0, 0]
   let trees = 0
-  for (let i = 1; i < height; i++) {
-    current = [(current[0] + 3) % width, current[1] + 1]
-    if (data[i][current[0]] === '#') {
+  do {
+    current = [(current[0] + slope[0]) % width, current[1] + slope[1]]
+    if (data[current[1]][current[0]] === '#') {
       trees++
     }
-  }
+  } while (current[1] !== height - 1)
   return trees
 }
 
 export function part2(input: string): number {
-  return 0
+  const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ]
+  return slopes.reduce((acc, cur) => acc * part1(input, cur), 1)
 }
 
 console.log('\x1b[31mDay 03')
