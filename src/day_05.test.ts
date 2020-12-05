62ece8f9551bb20a0a198a53beb3ca62ae8cdfdb
Andrius Simanaitis <eragonas5@gmail.com>
2020-12-05 14:33:45 +0200

    added day 05 part 1

---
+++ src/day_05.test.ts
@@ -0,0 +1,18 @@
+import { part2, decodeTicket } from './day_05'
+
+const input: [string, number][] = [
+  ['FBFBBFFRLR', 357],
+  ['BFFFBBFRRR', 567],
+  ['FFFBBBFRRR', 119],
+  ['BBFFBBFRLL', 820],
+]
+
+test('part 1', () => {
+  input.forEach((e) => {
+    expect(decodeTicket(e[0])).toBe(e[1])
+  })
+})
+
+test('part 2', () => {
+  expect(part2(input[0][0])).toBe(0)
+})
---
+++ src/day_05.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs'
+
+const input = fs.readFileSync(__dirname + '/day_05.txt', 'utf-8')
+
+export function decodeTicket(ticket: string) {
+  const parsed = Array.from(ticket.matchAll(/([FB]{7})([RL]{3})/g))[0]
+  const row = parseInt(parsed[1].replace(/F/g, '0').replace(/B/g, '1'), 2)
+  const seat = parseInt(parsed[2].replace(/L/g, '0').replace(/R/g, '1'), 2)
+  return +row * 8 + +seat
+}
+
+export function part1(input: string): number {
+  return Math.max(...input.split('\n').map((e) => decodeTicket(e)))
+}
+
+export function part2(input: string): number {
+  return 0
+}
+
+console.log('\x1b[31mDay 05')
+console.log('\x1b[0mPart 1:\n\x1b[32m' + part1(input))
+console.log('\x1b[0mPart 2:\n\x1b[32m' + part2(input) + '\x1b[33m')
