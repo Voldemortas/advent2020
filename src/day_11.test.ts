89314cafeef1a6f615bce3f937a96c0698767556
Andrius Simanaitis <eragonas5@gmail.com>
2020-12-19 23:49:04 +0200

    added day 11 part 2

--- src/day_11.test.ts
+++ src/day_11.test.ts
@@ -1,6 +1,7 @@
-import { part1, iterate, part2 } from './day_11'
+import { part1, iterate, part2, iterate2 } from './day_11'
 
-const input0 = `L.LL.LL.LL
+test('part 1', () => {
+  const input0 = `L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -11,7 +12,7 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL`
 
-const input1 = `#.##.##.##
+  const input1 = `#.##.##.##
 #######.##
 #.#.#..#..
 ####.##.##
@@ -22,7 +23,7 @@ const input1 = `#.##.##.##
 #.######.#
 #.#####.##`
 
-const input2 = `#.LL.L#.##
+  const input2 = `#.LL.L#.##
 #LLLLLL.L#
 L.L.L..L..
 #LLL.LL.L#
@@ -33,7 +34,7 @@ L.L.L..L..
 #.LLLLLL.L
 #.#LLLL.##`
 
-const input3 = `#.##.L#.##
+  const input3 = `#.##.L#.##
 #L###LL.L#
 L.#.#..#..
 #L##.##.L#
@@ -44,7 +45,7 @@ L.#.#..#..
 #.LL###L.L
 #.#L###.##`
 
-const input4 = `#.#L.L#.##
+  const input4 = `#.#L.L#.##
 #LLL#LL.L#
 L.L.L..#..
 #LLL.##.L#
@@ -55,7 +56,7 @@ L.L.L..#..
 #.LLLLLL.L
 #.#L#L#.##`
 
-const input5 = `#.#L.L#.##
+  const input5 = `#.#L.L#.##
 #LLL#LL.L#
 L.#.L..#..
 #L##.##.L#
@@ -66,7 +67,6 @@ L.#.L..#..
 #.LLLLLL.L
 #.#L#L#.##`
 
-test('part 1', () => {
   expect(iterate(input0)).toBe(input1)
   expect(iterate(input1)).toBe(input2)
   expect(iterate(input2)).toBe(input3)
@@ -78,5 +78,90 @@ test('part 1', () => {
 })
 
 test('part 2', () => {
-  expect(part2(input0)).toBe(0)
+  const input0 = `L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`
+
+  const input1 = `#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##`
+
+  const input2 = `#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#`
+
+  const input3 = `#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#`
+
+  const input4 = `#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#`
+
+  const input5 = `#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#`
+
+  const input6 = `#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#`
+
+  expect(iterate2(input0)).toBe(input1)
+  expect(iterate2(input1)).toBe(input2)
+  expect(iterate2(input2)).toBe(input3)
+  expect(iterate2(input3)).toBe(input4)
+  expect(iterate2(input4)).toBe(input5)
+  expect(iterate2(input5)).toBe(input6)
+  expect(iterate2(input6)).toBe(input6)
+  expect(part2(input0)).toBe(26)
+  expect(part2(input6)).toBe(26)
 })
--- src/day_11.ts
+++ src/day_11.ts
@@ -37,6 +37,50 @@ export function iterate(input: string): string {
     .join('\n')
 }
 
+export function iterate2(input: string): string {
+  const data: seats[][] = input.split('\n').map((e) => e.split('')) as seats[][]
+  function neighbours(y: number, x: number): seats[] {
+    function goDeep(horizontal: number, vertical: number): seats {
+      const temp = (data[y + vertical] ?? [])[x + horizontal] ?? []
+      if (typeof temp === 'object') {
+        return '.'
+      } else if (temp === '.') {
+        return goDeep(
+          (horizontal + Math.abs(horizontal) / horizontal) | 0,
+          (vertical + Math.abs(vertical) / vertical) | 0
+        )
+      } else {
+        return temp
+      }
+    }
+    return [
+      goDeep(-1, -1),
+      goDeep(0, -1),
+      goDeep(1, -1),
+
+      goDeep(-1, 0),
+      goDeep(1, 0),
+
+      goDeep(-1, 1),
+      goDeep(0, 1),
+      goDeep(1, 1),
+    ]
+  }
+  return data
+    .map((e, row) =>
+      e.map((i, col) =>
+        i === 'L' && !neighbours(row, col).some((e) => e === '#')
+          ? '#'
+          : i === '#' &&
+            neighbours(row, col).filter((e) => e === '#').length >= 5
+          ? 'L'
+          : i
+      )
+    )
+    .map((e) => e.join(''))
+    .join('\n')
+}
+
 export function part1(input: string): number {
   while (input != iterate(input)) {
     input = iterate(input)
@@ -45,7 +89,10 @@ export function part1(input: string): number {
 }
 
 export function part2(input: string): number {
-  return 0
+  while (input != iterate2(input)) {
+    input = iterate2(input)
+  }
+  return input.split('').filter((e) => e === '#').length
 }
 
 console.log('\x1b[31mDay 11')
